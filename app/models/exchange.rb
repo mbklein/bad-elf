f0bd1ba5681772cc3e23b69f963f6384260a43a6
Michael Klein <mbklein@gmail.com>
2014-11-13 16:45:40 -0600

    Generate better invite code

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -4,14 +4,19 @@ class Exchange < ActiveRecord::Base
   has_many :assignments, dependent: :destroy
   belongs_to :owner, class_name: "User"
   
-  after_initialize :generate_invite_code
+  before_save :generate_invite_code
   
   def self.for_user user
     user.nil? ?  Exchange.none : (Exchange.where(owner_id: user.id) + Assignment.where(elf_id: user.id).collect(&:exchange)).uniq
   end
   
   def generate_invite_code
-    self.invite_code ||= Digest::SHA512.new.to_s[0..9]
+    unless self.invite_code.present?
+      sha = Digest::SHA512.new
+      sha << Time.now.to_s
+      self.invite_code = sha.to_s[0..9]
+    end
+    
   end
   
   def participants
