e0e2c0459d972f916d48f9601d3fc6cb457df337
Michael Klein <mbklein@gmail.com>
2016-12-06 21:36:35 -0600

    Add glyph() helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
   def display_title
     @display_title || t('project')
   end
+  
+  def glyph(*names)
+    options = (names.last.kind_of?(Hash)) ? names.pop : {}
+    names.map! { |name| name.to_s.gsub('_','-') }
+    names.map! do |name|
+      name =~ /pull-(?:left|right)/ ? name : "glyphicon glyphicon-#{name}"
+    end
+    options[:tag] = options[:tag] ||= :i
+    content_tag options[:tag], nil, :class => names
+  end
 end
