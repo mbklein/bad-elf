e20c77c5ac395c1e126e428efee080c979761a53
Michael Klein <mbklein@gmail.com>
2016-12-06 21:26:37 -0600

    Fix bootstrap import/require statements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap-sprockets
 //= require bootstrap-datepicker
 //= require turbolinks
-//= require bootstrap
 //= require_tree .
