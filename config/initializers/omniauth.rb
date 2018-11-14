e507b5fa92f654e7bb78b66504190af26043584e
Michael Klein <mbklein@gmail.com>
2018-11-14 16:18:02 -0600

    Update for new Facebook permissions requirements
    Co-authored-by: Brendan Quinn <brendan-quinn@northwestern.edu>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook,      ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'] if ENV['FACEBOOK_KEY']
+  provider :facebook,      ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'default,email' if ENV['FACEBOOK_KEY']
   provider :google_oauth2, ENV["GOOGLE_KEY"],   ENV["GOOGLE_SECRET"]   if ENV['GOOGLE_KEY']
 end
